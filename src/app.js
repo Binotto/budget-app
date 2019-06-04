fedc30cd66fe56e69ed0da409e9fd145aa9ec661
Matheus <Binotto>
2019-06-04 18:47:51 -0300

    Correcting the error - Importing a new Component - AppRouter

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppRouter from './components/AppRouter';
+import AppRouter from './router/AppRouter';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
