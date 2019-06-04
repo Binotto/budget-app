7e7ec3be7751a7c93ca87713bbf55f0af2832883
Matheus <Binotto>
2019-06-04 16:52:28 -0300

    Linking Between Routes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {BrowserRouter, Route, Switch, Link} from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
@@ -27,20 +27,29 @@ const HelpPage = () => (
 
 const NotFoundPage = () => (
     <div>
-        404!
+        404! <link to="/">Go Home</link>
     </div>
+);
+
+const Header = () => (
+    <header>
+        <h1>Expensify</h1>
+    </header>
 );
 
 
 const routes = (
-    <BrowserRouter> 
-        <Switch>
-            <Route path="/" component={ExpenseDashboardPage} exact={true}/>
-            <Route path="/create" component={AddExpensePage}/>
-            <Route path="/edit" component={EditExpensePage} />
-            <Route path="/help" component={HelpPage} />
-            <Route component={NotFoundPage}/>
-        </Switch>   
+    <BrowserRouter>
+        <div>
+            <Header /> 
+            <Switch>
+                <Route path="/" component={ExpenseDashboardPage} exact={true}/>
+                <Route path="/create" component={AddExpensePage}/>
+                <Route path="/edit" component={EditExpensePage} />
+                <Route path="/help" component={HelpPage} />
+                <Route component={NotFoundPage}/>
+            </Switch>  
+        </div> 
     </BrowserRouter>
 );
 ReactDOM.render(routes, document.getElementById('app'));
