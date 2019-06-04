8ff714f1e48d7e2f681671082d046c4c0cf756c8
Matheus <Binotto>
2019-06-04 18:46:05 -0300

    Importing a new Component - AppRouter

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import AppRouter from './components/AppRouter';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(routes, document.getElementById('app'));
+ReactDOM.render(<AppRouter />, document.getElementById('app'));
