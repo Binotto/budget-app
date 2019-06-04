3e34525f5efb989496b4268445832a97bda814ff
Matheus <Binotto>
2019-06-04 15:28:10 -0300

    Importing Browser Routerand Route from React Router Dom

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
