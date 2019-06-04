bab99e13a2f10c843ffc828978b45ee42aed2e2b
Matheus <Binotto>
2019-06-04 19:44:50 -0300

    Creating a new Component - Header

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => (
+    <header>
+        <h1>Expensify</h1>
+        <NavLink to="/" activeClassName="is-active" exact={true}>Home Page </NavLink>
+        <NavLink to="/create" activeClassName="is-active">Create Page</NavLink>
+        <NavLink to="/edit" activeClassName="is-active">Edit Page</NavLink>
+        <NavLink to="/help" activeClassName="is-active">Help Page</NavLink>
+    </header>
+);
