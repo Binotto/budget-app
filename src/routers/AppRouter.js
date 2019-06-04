6785df6a6a5cf9b35533f75a3e596eea6ca1491d
Matheus <Binotto>
2019-06-04 19:46:02 -0300

    Importing a new Component - Header

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -5,17 +5,7 @@ import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
 import HelpPage from '../components/HelpPage';
 import NotFoundPage from '../components/NotFoundPage';
-
-
-const Header = () => (
-    <header>
-        <h1>Expensify</h1>
-        <NavLink to="/" activeClassName="is-active" exact={true}>Home Page </NavLink>
-        <NavLink to="/create" activeClassName="is-active">Create Page</NavLink>
-        <NavLink to="/edit" activeClassName="is-active">Edit Page</NavLink>
-        <NavLink to="/help" activeClassName="is-active">Help Page</NavLink>
-    </header>
-);
+import Header from '../components/Header';
 
 
 const AppRouter = () => (
