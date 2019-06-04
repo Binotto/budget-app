d4cc5ddf563857b86111cfda5c028b356f71b02b
Matheus <Binotto>
2019-06-04 20:25:50 -0300

    Query Strings and URL Parameters

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,6 @@ const Header = () => (
         <h1>Expensify</h1>
         <NavLink to="/" activeClassName="is-active" exact={true}>Home Page </NavLink>
         <NavLink to="/create" activeClassName="is-active">Create Page</NavLink>
-        <NavLink to="/edit" activeClassName="is-active">Edit Page</NavLink>
         <NavLink to="/help" activeClassName="is-active">Help Page</NavLink>
     </header>
 );
