7fb9fb9e80009c947c08bb2e1e278f624ae0fc23
Matheus <Binotto>
2019-06-06 15:08:10 -0300

    ES6 Object Destructuring X

--- playground/destructuring.js
+++ playground/destructuring.js
@@ -1,22 +1,35 @@
-const person = {
-    name: 'Matheus Binotto',
-    age: 25,
-    location: {
-        city: 'Curitiba',
-        temp: 70,
+// const person = {
+//     name: 'Matheus Binotto',
+//     age: 25,
+//     location: {
+//         city: 'Curitiba',
+//         temp: 70,
+//     }
+// };
+
+
+// const {name: firstName = 'Anonymous', age} = person;
+// // const name = person.name;
+// // const age = person.age;
+
+// console.log(`${firstName} is ${age}.`);
+
+// const {city, temp: temperature } = person.location;
+
+// if(city && temperature){
+//     console.log(`It's ${temperature} ${city}`);
+// }
+
+
+
+const book = {
+    title: 'Ego is the Enemy',
+    author: 'Ryan Holiday',
+    publisher: {
+        //name: 'Penguin'
     }
-};
-
-
-const {name: firstName = 'Anonymous', age} = person;
-// const name = person.name;
-// const age = person.age;
-
-console.log(`${firstName} is ${age}.`);
-
-const {city, temp: temperature } = person.location;
-
-if(city && temperature){
-    console.log(`It's ${temperature} ${city}`);
 }
 
+const { name: publisherName = 'Self-Published' } = book.publisher
+
+console.log(publisherName); // Penguin
