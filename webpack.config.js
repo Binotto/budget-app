d675a9836cfa850feeeb2365bef5635fe3e29dc7
Matheus <Binotto>
2019-06-05 21:11:25 -0300

    Setting up Redux

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = (env) => {
     const isProduction = env === 'production';
     return {
         mode: 'production',
-        entry: './src/app.js',
+        entry: './playground/redux-101.js',
         output:{
             path: path.join(__dirname, 'public/'),
             filename: 'bundle.js'
