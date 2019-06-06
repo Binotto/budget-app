6031ab15eddb2efd902303481cc4df6a6c0dc9c4
Matheus <Binotto>
2019-06-06 15:15:12 -0300

    ES6 Array Destructuring I

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = (env) => {
     const isProduction = env === 'production';
     return {
         mode: 'production',
-        entry: './playground/destructuring.js',
+        entry: './playground/array-destructuring.js',
         output:{
             path: path.join(__dirname, 'public/'),
             filename: 'bundle.js'
