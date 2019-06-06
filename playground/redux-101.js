67bb6737375ffda681370e8a4d65a069edbdfdd5
Matheus <Binotto>
2019-06-06 14:25:28 -0300

    Subscribing and Dynamic Actions III

--- playground/redux-101.js
+++ playground/redux-101.js
@@ -9,8 +9,9 @@ const store = createStore((state = { count: 0}, action) => {
                 count: state.count + incrementBy
             };
         case 'DECREMENT':
+            const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy: -1;
             return {
-                count: state.count - 1
+                count: state.count - decrementBy
             };
         case 'RESET':
             return {
@@ -35,6 +36,10 @@ store.dispatch({
 });
 store.dispatch({
     type: 'INCREMENT'
+});
+store.dispatch({
+    type: 'DECREMENT',
+    decrementBy: 10
 });
 store.dispatch({
     type: 'DECREMENT'
