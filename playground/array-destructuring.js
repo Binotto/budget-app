8c93b4d63094bd04bc296a79425402eaa3852769
Matheus <Binotto>
2019-06-06 15:25:32 -0300

    ES6 Array Destructuring II

--- playground/array-destructuring.js
+++ playground/array-destructuring.js
@@ -1,1 +1,7 @@
-console.log('array-destructuring');
+//
+//Array destructuring
+//
+
+const address = ['Jupiter Street', 'Philadelphia', 'Pensylvania'];
+
+console.log(`You are in ${address[0]} ${address[2]}.`);
