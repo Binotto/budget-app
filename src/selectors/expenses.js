950cf8917ba9a5c281558c5750c2d41bcae4b9c0
Matheus <Binotto>
2019-06-08 16:04:21 -0300

    Creating a file for Get visible expenses

---
+++ src/selectors/expenses.js
@@ -0,0 +1,16 @@
+//Get visible expenses
+export default (expenses, {text, sortBy, startDate, endDate}) => {
+    return expenses.filter((expense) => {
+        const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate;
+        const endDateMatch = typeof endDate !== 'numvber' || expense.createdAt <= endDate;
+        const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
+
+        return startDateMatch && endDateMatch && textMatch
+    }).sort((a, b) => {
+        if(sortBy === 'date'){
+            return a.createdAt < b.createdAt ? 1 : -1;
+        }else if (sortBy === 'amount'){
+            return a.amount < b.amount ? 1 : -1;
+        }
+    });
+}
