0d08491a5a1b2e3227594b54da0c99b479e6c072
Matheus <Binotto>
2019-06-08 16:17:26 -0300

    Importing the configureStore

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore'
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
