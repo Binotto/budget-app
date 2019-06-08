5025b461e80c2b154550315c1e8a0dd01f6cb93b
Matheus <Binotto>
2019-06-08 16:26:41 -0300

    Importing -AddExpenses, SetTextFilter, GetVisibleExpenses

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
-import configureStore from './store/configureStore'
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+
+const store = configureStore();
+
+console.log(store.getState());
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
