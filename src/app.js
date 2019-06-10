a660663544c4fdf7b0213c1874f9f613b7101f94
Matheus <Binotto>
2019-06-10 16:51:55 -0300

    Dropdown for Picking SortBy

--- src/app.js
+++ src/app.js
@@ -12,7 +12,8 @@ import './styles/styles.scss';
 const store = configureStore();
 
 store.dispatch(addExpense({description: 'Water bill', amount: 4500}));
-store.dispatch(addExpense({description: 'Gas bill'}));
+store.dispatch(addExpense({description: 'Gas bill', createdAt: 1000}));
+store.dispatch(addExpense({description: 'Rent', amount: 7700}));
 store.dispatch(setTextFilter('teste'));
 
 setTimeout(() => {
