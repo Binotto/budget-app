599a01a5ae045fc61991bbe99ef998a45d33ba4e
Matheus <Binotto>
2019-06-21 18:56:34 -0300

    Enzyme II

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import toJSON from 'enzyme-to-json';
 import Header from '../../components/Header';
 
 test('should render Header correctly', () => {
   const wrapper = shallow(<Header />);
-  expect(toJSON(wrapper)).toMatchSnapshot();
+  expect(wrapper).toMatchSnapshot();
 
   //expect(wrapper.find('h1').text()).toBe(Expensify);
   // const renderer = new ReactShallowRenderer();
